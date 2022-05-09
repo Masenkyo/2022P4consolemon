9048a3becc13343d4c02bf01a1c9425a4c22e236
Masenkyo <33390@ma-web.nl>
2022-05-09 14:33:34 +0200

    opdracht 1 af

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    class ConsoleMon
+    {
+        public int health;
+        public int energy;
+        public string name;
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+        }
+        public void DeleteEnergy(int energy)
+        {
+            this.energy -= energy;
+        }
+    }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    class Skill
+    {
+        public int damage;
+        public int energyCost;
+        public string name;
+
+        void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            caster.DeleteEnergy(energyCost);
+            target.TakeDamage(damage);
+        }
+    }
+}
